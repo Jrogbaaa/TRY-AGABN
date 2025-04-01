f9aafa9bebea4dd65faed0663c0805a16c1dbd40
Jrogbaaa <11jellis@gmail.com>
2025-04-01 15:49:56 +0200

    Update deployment configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lead-analyzer/', // Repository name for GitHub Pages
+  base: '/TRY-AGABN/', // Repository name for GitHub Pages
 })
